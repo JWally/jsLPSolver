64a02cdf3192020319f33b04b0bf9e752326f3ae
JustinWolcott <Justin.Wolcott@bbvacompass.com>
2013-11-27 13:15:46 -0600

    added puzzle to options

--- demos/demo_Options.js
+++ demos/demo_Options.js
@@ -75,3 +75,40 @@ var model = {
 }
 
 console.log(solver.Solve(model, 3));
+
+
+/*
+ http://mattmcd.github.io/2013/03/30/FX-Arbitrage-CLP.html
+ http://fx.priceonomics.com/v1/rates/
+ http://priceonomics.com/jobs/puzzle/
+*/
+
+
+var model = {
+    optimize: "d",
+    opType: "max",
+    constraints: {
+        d_1: {max: 1, min: 1},
+        e_1: {max: 0, min: 0},
+        p_1: {max: 0, min: 0},
+        y_1: {max: 0, min: 0},
+        d: {max: 10000, min: 1.1}
+    },
+    variables: {
+        d: {d_1: 1},
+        de: {d_1: 1, e_1: -1.1486},
+        dp: {d_1: 1, p_1: -0.7003},
+        dy: {d_1: 1, y_1: -133.33},
+        ed: {e_1: 1, d_1: -0.8706},
+        ep: {e_1: 1, p_1: -0.6097},
+        ey: {e_1: 1, y_1: -116.14},
+        pd: {p_1: 1, d_1: -1.4279},
+        pe: {p_1: 1, e_1: -1.6401},
+        py: {p_1: 1, y_1: -190.480},
+        yd: {y_1: 1, d_1: -0.0075},
+        yp: {y_1: 1, p_1: -0.00525},
+        ye: {y_1: 1, e_1: -0.00861}    
+    }
+};
+
+console.log(solver.Solve(model, 2));
