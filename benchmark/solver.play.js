d2946bd7aa0e4a1b06af9c6153613112f4f5014d
JustinWolcott <Justin.Wolcott@bbvacompass.com>
2015-04-29 12:22:04 -0500

    changed up speed test output

--- benchmark/solver.play.js
+++ benchmark/solver.play.js
@@ -14,12 +14,47 @@ console.log("------------------------");
 console.log("-FORWARD-");
 console.log("------------------------");
 
+var log = {};
+
+
 for (var i = 0; i < problems.length; i++) {
 
-    var j = problems[i];
+    var k = 0,
+        j = problems[i];
+        
+        
+        log[j.name] = {};
+        
+        for(var constraint in j.constraints){
+            if(j.constraints[constraint].max){
+                log[j.name].constraints = log[j.name].constraints  || 0;
+                log[j.name].constraints++;
+            }
+            
+            if(j.constraints[constraint].min){
+                log[j.name].constraints = log[j.name].constraints  || 0;
+                log[j.name].constraints++;
+            }
+        }
+        
+        log[j.name].variables = Object.keys(j.variables).length;
 
+        if(j.ints){
+            log[j.name].ints = Object.keys(j.ints).length;        
+        }
+
+
+
+}
+
+for( i = 0; i < problems.length; i++){
+    j = problems[i];
     var date_0 = process.hrtime();
     var d = solver.Solve(j);
     var a = process.hrtime(date_0);
-    console.log(problems[i].name, "--------->", a[0] + "s", a[1].toExponential(),"|||", d.result);
+    
+    log[j.name].result = d.result;
+    log[j.name].time =  a[0] + a[1] / 1e9;    
 }
+
+console.log(log);
