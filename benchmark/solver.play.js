ffd073ab3d8962f713915930780fe3ae1fe31496
bchevalier <bchevalier@wizcorp.jp>
2015-09-26 12:13:03 +0900

    * Undid changes to solver.play file

--- benchmark/solver.play.js
+++ benchmark/solver.play.js
@@ -9,12 +9,6 @@ var problems = require("../test/all_problems.json"),
     fs = require("fs"),
     solver = require("../src/solver");
 
-// var bigMILP = require('./test_suite/SPY_SPY_SPY_20170616.json');
-
-// bigMILP.name = 'SPY_SPY_SPY_20170616';
-// delete bigMILP.ints;
-// problems.push(bigMILP);
-
 
 console.log("------------------------");
 console.log("-FORWARD-");
@@ -51,86 +45,16 @@ for (var i = 0; i < problems.length; i++) {
 
 }
 
-var totalTime = 0;
-
-// var nProblems = problems.length;
-// var currentProblem = 0;
-
-// function RunNextTest() {
-//     var problemExecuted = false;
-//     var problem = problems[currentProblem];
-//     // if (problem.name === 'Monster Problem') {
-//     if (problem.name !== 'monster_II') {
-//         var date_0 = process.hrtime();
-//         var d = solver.Solve(problem);
-//         var a = process.hrtime(date_0);
-
-//         log[problem.name].result = d.result;
-//         log[problem.name].time =  a[0] + a[1] / 1e9;
-//         log[problem.name].iter = d.iter ? d.iter : undefined;
-//         log[problem.name].solution = d;
-
-//         totalTime += a[0] + a[1] / 1e9;
-//         problemExecuted = true;
-//     }
-
-//     currentProblem += 1;
-//     if (currentProblem < nProblems) {
-//         if (problemExecuted === true) {
-//             setTimeout(RunNextTest, 2000);
-//         } else {
-//             RunNextTest();
-//         }
-//     } else {
-//         // This is the end
-//         console.log(log);
-//         console.log('All Solved in', totalTime, 'seconds');
-//     }
-// }
-
-// RunNextTest();
-
-for(i = 0; i < problems.length; i++) {
-// for(i = problems.length - 1; i >= 0; i--){
-    problem = problems[i];
-
-    // if (problem.name === 'monster_II') {
-    // if (problem.name !== 'SPY_SPY_SPY_20170616') {
-    // if (problem.name !== 'monster_II') {
-    // if (problem.name !== 'Monster Problem') {
-    // if (problem.name === 'monster_II' || problem.name === 'Monster Problem') {
-    // if (problem.name !== 'Berlin Air Lift Problem') {
-    // if (problem.name !== 'Integer Berlin Air Lift Problem') {
-    // if (problem.name !== 'Infeasible Berlin Air Lift Problem') {
-    // if (problem.name !== 'Generic Business Problem 2') {
-    // if (problem.name !== 'Wiki 1') {
-    // if (problem.name !== 'Artificial') {
-    // if (problem.name !== 'Coffee Problem') {
-    // if (problem.name !== 'Shift Work Problem') {
-    // if (problem.name !== 'Degenerate Max') {
-    // if (problem.name !== 'Degenerate Min') {
-    if (problem.name !== 'Integer Wood Problem') {
-    // if (problem.name !== 'Chevalier 1') {
-    // if (problem.name !== 'Integer Chocolate Problem') {
-    // if (problem.name !== 'Integer Clothing Shop Problem II') {
-        continue;
-    }
-console.log('>>>>> Solving', problem.name, '<<<<<');
+for( i = 0; i < problems.length; i++){
+    j = problems[i];
     var date_0 = process.hrtime();
-    var d = solver.Solve(problem);
+    var d = solver.Solve(j);
     var a = process.hrtime(date_0);
 
-    log[problem.name].objectiveValue = d.objectiveValue;
-    log[problem.name].time =  a[0] + a[1] / 1e9;
-    log[problem.name].iter = d.iter ? d.iter : undefined;
-    log[problem.name].solutionSet = d.solutionSet;
-    log[problem.name].feasible = d.feasible;
-// console.log('Result', d.result);
-// console.log('>>>>> Solved in', (a[0] + a[1] / 1e9) * 1000, 'ms <<<<<');
-// console.log('');
+    log[j.name].result = d.result;
+    log[j.name].time =  a[0] + a[1] / 1e9;
+    log[j.name].iter = d.iter ? d.iter : undefined;
 
-    totalTime += a[0] + a[1] / 1e9;
 }
 
 console.log(log);
-console.log('All Solved in', totalTime, 'seconds');
