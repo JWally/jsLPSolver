4a4f2e348b1a038290b015cc113e35a44144b0f1
JustinWolcott <Justin.Wolcott@bbvacompass.com>
2015-10-05 10:30:02 -0500

    added rounding ability to test models

--- test/solver.problems.js
+++ test/solver.problems.js
@@ -7,15 +7,24 @@ var assert = require("assert"),
 
 problems = problems.concat(require("./all_problems.json"));
 
+// Kick off the second "Monster" problem
 problems.splice(-1,1);
 
 // For testing, Function to sort an object
-function sortObject(theObj) {
+function sortObject(theObj, round) {
     var finalObj = {};
 
     Object.keys(theObj).sort()
         .forEach(function (d) {
-            finalObj[d] = theObj[d];
+            if(round){
+                if(!isNaN(theObj[d])){
+                    finalObj[d] = Math.round(theObj[d] * Math.pow(10,round)) / (Math.pow(10,round));  
+                } else {
+                    finalObj[d] = theObj[d];                
+                }
+            } else {
+                finalObj[d] = theObj[d];
+            }
         });
 
     return finalObj;
@@ -33,8 +42,8 @@ describe("The Solve method takes a problem and solves it",
                         b = solver.Solve(d);
 
                     assert.deepEqual(
-                        sortObject(b),
-                        sortObject(a)
+                        sortObject(b,5),
+                        sortObject(a,5)
 
                     );
                 });
