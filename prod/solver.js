4599e065c1d01bcf0f2eb1c0bb678eba723820a6
JustinWolcott <Justin.Wolcott@bbvacompass.com>
2015-10-28 12:24:24 -0500

    JW: Build prod version of solver

--- prod/solver.js
+++ prod/solver.js
@@ -6,6 +6,182 @@
 /*global it*/
 /*global console*/
 /*global process*/
+ 
+ 
+ /*************************************************************
+ * Method: ReformatLP
+ * Scope: Public:
+ * Agruments: model: The model we want solver to operate on
+ * Purpose: Convert a friendly JSON model into a model for a
+ *          real solving library...in this case
+ *          lp_solver
+ **************************************************************/ 
+module.exports = function (model, fx) {
+        // Make sure we at least have a model
+        if (!model) {
+            throw new Error("Solver requires a model to operate on");
+        }
+
+        var constraints, variables, hash, len, i, j, x, c, v,
+            rslts, tmpRow, table, val,
+            tall = 1,
+            wide = 1,
+            z = 0,
+            opType = model.opType === "max" ? -1 : 1,
+            rxClean = new RegExp("[^A-Za-z0-9]+", "gi");
+
+
+        //Give all of the variables a self property of 1
+        for (v in model.variables) {
+            model.variables[v][v] = 1;
+            //if a min or max exists in the variables;
+            //add it to the constraints
+            if (typeof model.variables[v].max !== "undefined") {
+                model.constraints[v] = model.constraints[v] || {};
+                model.constraints[v].max = model.variables[v].max;
+            }
+
+            if (typeof model.variables[v].min !== "undefined") {
+                model.constraints[v] = model.constraints[v] || {};
+                model.constraints[v].min = model.variables[v].min;
+            }
+        }
+
+        constraints = Object.keys(model.constraints); //Array with name of each constraint type
+        variables = Object.keys(model.variables); //Array with name of each Variable
+
+        // FIGURE OUT HEIGHT
+        for (x in model.constraints) {
+            if (typeof model.constraints[x].min !== "undefined") {
+                model.constraints[x].min_loc = tall - 1;
+                tall += 1;
+            }
+
+            if (typeof model.constraints[x].max !== "undefined") {
+                model.constraints[x].max_loc = tall - 1;
+                tall += 1;
+            }
+        }
+
+        // FIGURE OUT WIDTH
+        wide += variables.length;
+
+        // BUILD A FAKE ROW OF THAT WIDTH
+        tmpRow = new Array(wide);
+
+        // BUILD AN EMPTY TABLEAU
+        /* jshint ignore:start */
+        table = new Array(tall);
+        for (i = 0; i < tall; i++) {
+            table[i] = []; //tmpRow.slice();
+        }
+        /* jshint ignore:end */
+
+        // LOOP IT AGAIN!!!
+        z = 0;
+        for (x in model.constraints) {
+            if (typeof model.constraints[x].min !== "undefined") {
+                // DO RHS
+                table[z][wide - 1] = " >= " + model.constraints[x].min;
+                // COUNTER += 1...
+                z += 1;
+            }
+
+            if (typeof model.constraints[x].max !== "undefined") {
+                // DO RHS
+                table[z][wide - 1] = " <= " + model.constraints[x].max;
+                // COUNTER += 1...
+                z += 1;
+            }
+        }
+
+        // TRY LOADING THE TABLE
+        for (v in model.variables) {
+            // Get the column's location
+            var col = variables.indexOf(v);
+            for (var a in model.variables[v]) {
+                // This is the thing we're trying to optimize...
+                if (a === model.optimize) {
+                    val = model.variables[v][a];
+                    if (col !== 0) {
+                        if (val > 0) {
+                            val = " + " + val;
+                        } else {
+                            val = " - " + -val;
+                        }
+                    }
+                    table[tall - 1][col] = val + " ";
+                    table[tall - 1][col] += v.replace(rxClean, "_");
+                } else if (typeof model.constraints[a] !== "undefined") {
+                    var row,
+                        cns = model.constraints[a];
+
+                    if (typeof cns.min !== "undefined") {
+                        row = cns.min_loc;
+                        val = model.variables[v][a];
+                        if (col !== 0) {
+                            if (val > 0) {
+                                val = " + " + val;
+                            } else {
+                                val = " - " + -val;
+                            }
+                        }
+                        table[row][col] = val;
+                        table[row][col] += " ";
+                        table[row][col] += v.replace(rxClean, "_");
+                    }
+
+                    if (typeof cns.max !== "undefined") {
+                        row = cns.max_loc;
+                        val = model.variables[v][a];
+                        if (col !== 0) {
+                            if (val > 0) {
+                                val = " + " + val;
+                            } else {
+                                val = " - " + -val;
+                            }
+                        }
+                        table[row][col] = val;
+                        table[row][col] += " ";
+                        table[row][col] += v.replace(rxClean, "_");
+                    }
+                }
+            }
+        }
+
+        var outTbl = [];
+
+        // ADD ALL OF THE EQUATIONS TO THE NEW TABLE
+        // IN REVERSE (SINCE MY OBJECTIVE ROW IS AT BOTTOM)
+        // AND JOIN THEM ALL TOGETHER, LAZILY CLEANING
+        // IT UP AS WE GO
+        for (i = table.length - 1; i >= 0; i--) {
+            outTbl.push((table[i].join("") + ";")
+                .replace(/^ \+ /gi, ""));
+        }
+
+        // BECAUSE IT SEEMS TO SCREW UP...
+        outTbl[0] = model.opType + ": " + outTbl[0];
+
+        // ADD INTS IF THERE ARE IN FACT ANY...
+        if (model.ints) {
+            var ary = Object.keys(model.ints);
+            // Push in a blank row to the rslts array
+            outTbl.push("");
+            for (i = 0; i < ary.length; i++) {
+                outTbl.push("int " + ary[i].replace(/[^A-Za-z0-9]+/gi,
+                    "_") + ";");
+            }
+        }
+        return outTbl;
+    };
+},{}],2:[function(require,module,exports){
+/*global describe*/
+/*global require*/
+/*global module*/
+/*global it*/
+/*global console*/
+/*global process*/
 
 //-------------------------------------------------------------------
 //-------------------------------------------------------------------
@@ -180,8 +356,6 @@ function MILP(model) {
             // Branches with the most promising lower bounds
             // will be picked first
             branches.sort(sortByEvaluation);
-            // branches.sort(sortByNbIntegers);
-            // branches.sort(sortAdvanced);
         }
     }
 
@@ -193,7 +367,7 @@ function MILP(model) {
 }
 module.exports = MILP;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 /*global describe*/
 /*global require*/
 /*global module*/
@@ -388,7 +562,7 @@ Model.prototype.setCost = function (cost, variable) {
 //-------------------------------------------------------------------
 //-------------------------------------------------------------------
 Model.prototype.loadJson = function (jsonModel) {
-    this.isMinimization = (jsonModel.opType === "min");
+    this.isMinimization = (jsonModel.opType !== "max");
 
     var variables = jsonModel.variables;
     var constraints = jsonModel.constraints;
@@ -412,7 +586,15 @@ Model.prototype.loadJson = function (jsonModel) {
 
         var max = (equal === undefined) ? constraint.max : equal;
         if (max !== undefined) {
-            constraintsMax[constraintId] =  this.smallerThan(max);
+            constraintsMax[constraintId] = this.smallerThan(max);
+        }
+
+        if (equal !== undefined) {
+            var equality = new Equality(constraintsMin[constraintId], constraintsMax[constraintId]);
+            if (constraint.weight !== undefined) {
+                equality.relax(constraint.weight);
+            }
+            continue;
         }
     }
 
@@ -498,7 +680,7 @@ Model.prototype.log = function (message) {
     return this.tableau.log(message);
 };
 
-},{"./MILP.js":1,"./Tableau.js":3,"./expressions.js":4}],3:[function(require,module,exports){
+},{"./MILP.js":2,"./Tableau.js":4,"./expressions.js":5}],4:[function(require,module,exports){
 /*global describe*/
 /*global require*/
 /*global module*/
@@ -1522,7 +1704,7 @@ Tableau.prototype.log = function (message, force) {
     return this;
 };
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 /*global describe*/
 /*global require*/
 /*global module*/
@@ -1627,16 +1809,36 @@ Constraint.prototype.setVariableCoefficient = function (newCoefficient, variable
     return this;
 };
 
+var errorVarIdx = 1;
+Constraint.prototype.relax = function (weight) {
+    if (this.model.isMinimization === false) {
+        weight = -weight;
+    }
+
+    var error = this.model.addVariable(weight, "e" + (errorVarIdx++).toString());
+    this._relax(error);
+};
+
+Constraint.prototype._relax = function (error) {
+    if (this.isUpperBound) {
+        this.setVariableCoefficient(-1, error);
+    } else {
+        this.setVariableCoefficient(1, error);
+    }
+};
+
 //-------------------------------------------------------------------
 //-------------------------------------------------------------------
 function Equality(constraintUpper, constraintLower) {
     this.upperBound = constraintUpper;
     this.lowerBound = constraintLower;
+    this.model = constraintUpper.model;
+    this.rhs = constraintUpper.rhs;
 }
 
-Equality.prototype.addTerm = function (term) {
-    this.upperBound.addTerm(term);
-    this.lowerBound.addTerm(term);
+Equality.prototype.addTerm = function (coefficient, variable) {
+    this.upperBound.addTerm(coefficient, variable);
+    this.lowerBound.addTerm(coefficient, variable);
     return this;
 };
 
@@ -1649,6 +1851,17 @@ Equality.prototype.removeTerm = function (term) {
 Equality.prototype.setRightHandSide = function (rhs) {
     this.upperBound.setRightHandSide(rhs);
     this.lowerBound.setRightHandSide(rhs);
+    this.rhs = rhs;
+};
+
+Equality.prototype.relax = function (weight) {
+    if (this.model.isMinimization === false) {
+        weight = -weight;
+    }
+
+    var error = this.model.addVariable(weight, "e" + (errorVarIdx++).toString());
+    this.upperBound._relax(error);
+    this.lowerBound._relax(error);
 };
 
 
@@ -1659,7 +1872,7 @@ module.exports = {
     Term: Term
 };
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 /*global describe*/
 /*global require*/
 /*global module*/
@@ -1750,189 +1963,23 @@ var Solver = function () {
     };
 
     /*************************************************************
-     * Method: Model
+     * Method: ReformatLP
      * Scope: Public:
      * Agruments: model: The model we want solver to operate on
      * Purpose: Convert a friendly JSON model into a model for a
      *          real solving library...in this case
      *          lp_solver
      **************************************************************/
-    this.ReformatLP = function (model, fx) {
-        // Make sure we at least have a model
-        if (!model) {
-            throw new Error("Solver requires a model to operate on");
-        }
-
-        var constraints, variables, hash, len, i, j, x, c, v,
-            rslts, tmpRow, table, val,
-            tall = 1,
-            wide = 1,
-            z = 0,
-            opType = model.opType === "max" ? -1 : 1,
-            rxClean = new RegExp("[^A-Za-z0-9]+", "gi");
-
-
-        //Give all of the variables a self property of 1
-        for (v in model.variables) {
-            model.variables[v][v] = 1;
-            //if a min or max exists in the variables;
-            //add it to the constraints
-            if (typeof model.variables[v].max !== "undefined") {
-                model.constraints[v] = model.constraints[v] || {};
-                model.constraints[v].max = model.variables[v].max;
-            }
-
-            if (typeof model.variables[v].min !== "undefined") {
-                model.constraints[v] = model.constraints[v] || {};
-                model.constraints[v].min = model.variables[v].min;
-            }
-        }
-
-        constraints = Object.keys(model.constraints); //Array with name of each constraint type
-        variables = Object.keys(model.variables); //Array with name of each Variable
-
-        // FIGURE OUT HEIGHT
-        for (x in model.constraints) {
-            if (typeof model.constraints[x].min !== "undefined") {
-                model.constraints[x].min_loc = tall - 1;
-                tall += 1;
-            }
-
-            if (typeof model.constraints[x].max !== "undefined") {
-                model.constraints[x].max_loc = tall - 1;
-                tall += 1;
-            }
-        }
-
-        // FIGURE OUT WIDTH
-        wide += variables.length;
-
-        // BUILD A FAKE ROW OF THAT WIDTH
-        tmpRow = new Array(wide);
-
-        // BUILD AN EMPTY TABLEAU
-        /* jshint ignore:start */
-        table = new Array(tall);
-        for (i = 0; i < tall; i++) {
-            table[i] = []; //tmpRow.slice();
-        }
-        /* jshint ignore:end */
-
-        // LOOP IT AGAIN!!!
-        z = 0;
-        for (x in model.constraints) {
-            if (typeof model.constraints[x].min !== "undefined") {
-                // DO RHS
-                table[z][wide - 1] = " >= " + model.constraints[x].min;
-                // COUNTER += 1...
-                z += 1;
-            }
-
-            if (typeof model.constraints[x].max !== "undefined") {
-                // DO RHS
-                table[z][wide - 1] = " <= " + model.constraints[x].max;
-                // COUNTER += 1...
-                z += 1;
-            }
-        }
-
-        // TRY LOADING THE TABLE
-        for (v in model.variables) {
-            // Get the column's location
-            var col = variables.indexOf(v);
-            for (var a in model.variables[v]) {
-                // This is the thing we're trying to optimize...
-                if (a === model.optimize) {
-                    val = model.variables[v][a];
-                    if (col !== 0) {
-                        if (val > 0) {
-                            val = " + " + val;
-                        } else {
-                            val = " - " + -val;
-                        }
-                    }
-                    table[tall - 1][col] = val + " ";
-                    table[tall - 1][col] += v.replace(rxClean, "_");
-                } else if (typeof model.constraints[a] !== "undefined") {
-                    var row,
-                        cns = model.constraints[a];
-
-                    if (typeof cns.min !== "undefined") {
-                        row = cns.min_loc;
-                        val = model.variables[v][a];
-                        if (col !== 0) {
-                            if (val > 0) {
-                                val = " + " + val;
-                            } else {
-                                val = " - " + -val;
-                            }
-                        }
-                        table[row][col] = val;
-                        table[row][col] += " ";
-                        table[row][col] += v.replace(rxClean, "_");
-                    }
-
-                    if (typeof cns.max !== "undefined") {
-                        row = cns.max_loc;
-                        val = model.variables[v][a];
-                        if (col !== 0) {
-                            if (val > 0) {
-                                val = " + " + val;
-                            } else {
-                                val = " - " + -val;
-                            }
-                        }
-                        table[row][col] = val;
-                        table[row][col] += " ";
-                        table[row][col] += v.replace(rxClean, "_");
-                    }
-                }
-            }
-        }
-
-        var outTbl = [];
-
-        // ADD ALL OF THE EQUATIONS TO THE NEW TABLE
-        // IN REVERSE (SINCE MY OBJECTIVE ROW IS AT BOTTOM)
-        // AND JOIN THEM ALL TOGETHER, LAZILY CLEANING
-        // IT UP AS WE GO
-        for (i = table.length - 1; i >= 0; i--) {
-            outTbl.push((table[i].join("") + ";")
-                .replace(/^ \+ /gi, ""));
-        }
-
-        // BECAUSE IT SEEMS TO SCREW UP...
-        outTbl[0] = model.opType + ": " + outTbl[0];
-
-        // ADD INTS IF THERE ARE IN FACT ANY...
-        if (model.ints) {
-            var ary = Object.keys(model.ints);
-            // Push in a blank row to the rslts array
-            outTbl.push("");
-            for (i = 0; i < ary.length; i++) {
-                outTbl.push("int " + ary[i].replace(/[^A-Za-z0-9]+/gi,
-                    "_") + ";");
-            }
-        }
-        return outTbl;
-    };
+    this.ReformatLP = require("./LP_Solve");
+    
+   
 };
 
 // Determine the environment we're in.
 // if we're in node, offer a friendly exports
 // otherwise, Solver's going global
 /* jshint ignore:start */
-/*
-(function () {
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = new Solver();
-    } else if (typeof define === "function") {
-        define([], function () {
-            return Solver;
-        });
-    }
-})();
-*/
+
 (function(){
     // If define exists; use it
     if (typeof define === "function") {       
@@ -1948,4 +1995,4 @@ var Solver = function () {
 
 /* jshint ignore:end */
 
-},{"./MILP":1,"./Model":2,"./Tableau":3,"./expressions.js":4}]},{},[5]);
+},{"./LP_Solve":1,"./MILP":2,"./Model":3,"./Tableau":4,"./expressions.js":5}]},{},[6]);
