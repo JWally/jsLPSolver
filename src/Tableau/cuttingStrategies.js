693e9fb7c9745d4d747e27d7fc0b1c91ab863c5f
Loic Venerosy <lvenerosy@wizcorp.jp>
2017-06-24 18:16:28 +0900

    remove broken mir cut

--- src/Tableau/cuttingStrategies.js
+++ src/Tableau/cuttingStrategies.js
@@ -116,67 +116,6 @@ Tableau.prototype._addLowerBoundMIRCut = function(rowIndex) {
 	return true;
 };
 
-Tableau.prototype._addUpperBoundMIRCut = function(rowIndex) {
-
-	if (rowIndex === this.costRowIndex) {
-		//console.log("! IN MIR CUTS : The index of the row corresponds to the cost row. !");
-		return false;
-	}
-
-	var model = this.model;
-	var matrix = this.matrix;
-
-	var intVar = this.variablesPerIndex[this.varIndexByRow[rowIndex]];
-	if (!intVar.isInteger) {
-		return false;
-    }
-
-	var b = matrix[rowIndex][this.rhsColumn];
-	var f = b - Math.ceil(b);
-
-	if (f < this.precision || 1 - this.precision < f) {
-		return false;
-    }
-
-	//Adding a row
-	var r = this.height;
-	matrix[r] = matrix[r - 1].slice();
-	this.height += 1;
-
-	// Creating slack variable
-	this.nVars += 1;
-	var slackVarIndex = this.getNewElementIndex();
-	this.varIndexByRow[r] = slackVarIndex;
-	this.rowByVarIndex[slackVarIndex] = r;
-	this.colByVarIndex[slackVarIndex] = -1;
-	this.variablesPerIndex[slackVarIndex] = new SlackVariable("s"+slackVarIndex, slackVarIndex);
-
-	matrix[r][this.rhsColumn] = -f;
-
-	for(var colIndex = 1; colIndex < this.varIndexByCol.length; colIndex += 1) {
-		var variable = this.variablesPerIndex[this.varIndexByCol[colIndex]];
-
-		var aj = matrix[rowIndex][colIndex];
-		var fj = aj - Math.floor(aj);
-
-		if(variable.isInteger) {
-			if(fj <= f) {
-				matrix[r][colIndex] = -fj;
-            } else {
-				matrix[r][colIndex] = -(1 - fj) * f / fj;
-            }
-		} else {
-			if (aj >= 0) {
-				matrix[r][colIndex] = -aj;
-            } else {
-				matrix[r][colIndex] = aj * f / (1 - f);
-            }
-		}
-	}
-
-	return true;
-};
-
 Tableau.prototype.applyMIRCuts = function () {
     var nRows = this.height;
     for (var cst = 0; cst < nRows; cst += 1) {
