29ea2b889b263c7d4a7be94714eaab94995f27f8
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2019-10-03 16:33:07 -0500

    made testing reporter spec because its easier for me to read

--- gruntfile.js
+++ gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt){
         "mochaTest": {
             "test": {
                 "options": {
-                    "reporter": "json",
+                    "reporter": "spec",
                     "quite": "false"
                 },
                 "src": ["test/solver.problems.js"]
