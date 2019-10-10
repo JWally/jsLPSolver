da9daaee418fb0526261dcc871f7a07ff862a2cb
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2019-10-10 09:43:40 -0500

    misc updates to testing suite

--- test/misc/_main_.js
+++ test/misc/_main_.js
@@ -4,44 +4,9 @@
 /*global it*/
 /*global console*/
 /*global process*/
-
 var solver = require("../../src/solver");
-var models = [];
+var model = require("../test-wip/SPY_SPY_SPY_20160617.json");
 
-models.push(require("../test-wip/LargeFarmMIP.json"));
-models.push(require("../test-wip/StockCuttingProblem.json"));
-models.push(require("../test-wip/ChenhuaWANG22_1.json"));
-models.push(require("../test-wip/ChenhuaWANG22_2.json"));
+var a = solver.Solve(model); 
+console.log(a);
 
-models.forEach(function(model){
-
-var solution = solver.Solve(model, 1e-9, true);
-
-    // Otherwise; give the user the bare
-    // minimum of info necessary to carry on
-
-    var store = {};
-
-    // 1.) Add in feasibility to store;
-    store.feasible = solution.feasible;
-
-    // 2.) Add in the objective value
-    store.result = solution.evaluation;
-
-    store.bounded = solution.bounded;
-    
-    if(solution._tableau.__isIntegral){
-        store.isIntegral = true;
-    }
-
-    // 3.) Load all of the variable values
-    Object.keys(solution.solutionSet)
-    .map(function (d) {
-        store[d] = solution.solutionSet[d];
-    });
-    
-    store.messages = solution._tableau.model.messages;
-
-    console.log(store);
-    
-});
