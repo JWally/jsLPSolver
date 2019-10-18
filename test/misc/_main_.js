46bd6da3bbd1ee167204054b72896716cdb6be01
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2019-10-18 05:52:30 -0500

    dumb example testing out lpsolve.exe against existing problem set(s)

--- test/misc/_main_.js
+++ test/misc/_main_.js
@@ -4,9 +4,66 @@
 /*global it*/
 /*global console*/
 /*global process*/
-var solver = require("../../src/solver");
-var model = require("../test-wip/SPY_SPY_SPY_20160617.json");
 
-var a = solver.Solve(model); 
-console.log(a);
+(async () => {
+
+    console.log("hey there");
+
+    var solver = require("../../src/solver");
+    var fs = require("fs");
+
+
+    var ary = fs.readdirSync("test/test-sanity/")
+                .filter(function(file){return /\.json$/.test(file);});
+
+
+    for(var i = 0; i < ary.length; i++){
+        
+        model = ary[i];
+        
+        model = fs.readFileSync("test/test-sanity/" + model, "utf8");
+
+        model = JSON.parse(model);
+
+        console.log("-----------------");
+        console.log("-----------------");
+        console.log(model.name)
+        console.log("-----------------");
+        console.log("-----------------");
+        
+        model["external"] = {
+            "solver": "lpsolve",
+            "binPath": "C:/lpsolve/lp_solve.exe",
+            "tempName": "C:/temp/out.txt",
+            "args": [
+                "-s2",
+                "-timeout",
+                8
+            ]
+        }
+        
+
+
+        await solver.Solve(model)
+            .then(data => console.log())
+            .catch(function(e){
+                for(j = 0; j < 1; j++){
+                    console.log(e);
+                    // 1 = timeout
+                    // 2 = infeasible
+                    // 3 = unbounded
+                    if(e.code != "2" && e.code != "3" && e.code != "1"){
+                        fs.renameSync("C:/temp/out.txt","C:/temp/" + model.name.replace(/[^A-Za-z0-9]/gi,"_") + ".txt");
+                    }
+                }
+
+                //throw new Error("STOP IT!!!");
+            });
+        
+    }
+
+
+
+})();
+
 
