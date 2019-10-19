fa110999a193873220a3ea8eead79f7be3cbceb1
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2019-10-18 22:24:29 -0500

    update test/misc/_main_ to be more useful for speed testing comps

--- test/misc/_main_.js
+++ test/misc/_main_.js
@@ -24,42 +24,44 @@
         model = fs.readFileSync("test/test-sanity/" + model, "utf8");
 
         model = JSON.parse(model);
-
-        console.log("-----------------");
-        console.log("-----------------");
-        console.log(model.name)
+        console.log("");
+        console.log("");
         console.log("-----------------");
         console.log("-----------------");
         
+        if(model.ints){int_count = Object.keys(model.ints).length} else {int_count = 0}
+        console.log(model.name,"[",Object.keys(model.variables).length, " variables,", Object.keys(model.constraints).length," constraints, ",int_count," integers ]")
         model["external"] = {
             "solver": "lpsolve",
             "binPath": "C:/lpsolve/lp_solve.exe",
             "tempName": "C:/temp/out.txt",
             "args": [
+                "-presolve",
+                "-presolver",
                 "-s2",
                 "-timeout",
-                8
+                8,
+                "-gr",
+                0.001
             ]
         }
         
 
 
+        console.time("lpsolve");
         await solver.Solve(model)
-            .then(data => console.log())
+            .then(function(data){
+                //fs.writeFileSync("C:/temp/" + model.name + ".json", JSON.stringify(data, null, "\t"));
+            })
             .catch(function(e){
-                for(j = 0; j < 1; j++){
-                    console.log(e);
-                    // 1 = timeout
-                    // 2 = infeasible
-                    // 3 = unbounded
-                    if(e.code != "2" && e.code != "3" && e.code != "1"){
-                        fs.renameSync("C:/temp/out.txt","C:/temp/" + model.name.replace(/[^A-Za-z0-9]/gi,"_") + ".txt");
-                    }
-                }
-
-                //throw new Error("STOP IT!!!");
+                //console.log(e)
+                //fs.renameSync("C:/temp/out.txt","C:/temp/error_" + model.name.replace(/[^A-Za-z0-9]/gi,"_") + ".txt");
             });
-        
+        console.timeEnd("lpsolve");
+        delete model.external;
+        console.time("jsLPSolver");
+        var a = solver.Solve(model);
+        console.timeEnd("jsLPSolver");
     }
 
 
